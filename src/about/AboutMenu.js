482b9b96dac0dceb6921f29988efc1e92b877b6c
doe1pq <tariqali30021@gmail.com>
2024-10-11 18:44:24 +0200

    Added menus

--- src/about/AboutMenu.js
+++ src/about/AboutMenu.js
@@ -1,9 +1,77 @@
 import React, { Component } from 'react'
+import AboutMenuItem from "./AboutMenuItem.js"
+import AboutSubheading from "./AboutSubheading.js"
+import SubheadingData from "./SubheadingData.js"
+import PersonalIcon from "../assets/moebius-triangle.png"
+import EducationIcon from "../assets/upgrade.png"
+import CareerIcon from "../assets/triple-corn.png"
 
 export default class AboutMenu extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      activeMenuItem:1,
+      activeSubheading:1,
+    };
+  };
+  
+  handleMenuItemClick = (menuItem) => {
+    this.setState({
+      activeMenuItem: menuItem,
+      activeSubheading:1,
+    });
+  };
+
+  handleSubheadingClick = (subheading) => {
+    this.setState({
+      activeSubheading:subheading,
+    });
+  };
+  
+  
+  
   render() {
+    const {activeMenuItem, activeSubheading} = this.state;
+    const menuItems = ["CAREER", "EDUCATION", "PERSONAL"];
+
+    const activeMenuTitle = menuItems[activeMenuItem-1]
+    const activeMenuIcon  = activeMenuTitle === "PERSONAL" ? PersonalIcon : activeMenuTitle === "EDUCATION" ? EducationIcon : CareerIcon
+
+    const subheadings = SubheadingData[activeMenuItem]
+
+
     return (
-      <div>AboutMenu</div>
+      <>
+    <div className="menu">
+      {menuItems.map((item, index) => (
+        <AboutMenuItem
+        key={index}
+        title={item}
+        active={activeMenuItem === index + 1}
+        onClick= {() => this.handleMenuItemClick(index+1)}
+        />
+      ))}
+    </div>
+    
+    <div className="sub-container">
+      <div className="icon-title-container">
+        <img src={activeMenuIcon} alt={activeMenuTitle} className="icon"/>
+        <h3>{activeMenuTitle}</h3>
+      </div>
+      {subheadings.map((subheading, index) => (
+        <AboutSubheading
+        key={index}
+        title={subheading.title}
+        content={subheading.content}
+        active={activeSubheading === index + 1}
+        onClick= {() => this.handleSubheadingClick(index+1)}
+        menuItem={activeMenuItem}
+        />
+      ))}
+    </div>
+    
+    </>  
     )
   }
 }
---
+++ src/about/SubheadingData.js
@@ -0,0 +1,84 @@
+const SubheadingData = {
+    3: [
+        {
+            title: "About Me",
+            content: (
+                <>
+                <p>
+                    I am so sick and tired of looking for a job im so good please
+                </p>
+                </>
+            ),
+        },
+    ],
+    2: [
+        {
+            title: "2023 Bachelor's Degree",
+            content: (
+                <>
+                <p>
+                    I am so sick and tired of looking for a job im so good please
+                </p>
+                </>
+            ),
+        },
+        {
+            title: "2024 Master's Degree",
+            content: (
+                <>
+                <p>
+                    I am so sick and tired of looking for a job im so good please
+                </p>
+                </>
+            ),
+        },
+    ],
+    1: [
+        {
+            title: "Backend Developer | Wise | Oct 2023 - July 2024 | Budapest, Hungary",
+            content: (
+                <>
+                <p>
+                ● Built and managed custom dashboards to track KPIs, leading to a 20% improvement in system uptime.
+                ● Managed incidents and gained experience with on-call rotations to ensure operation of the Wise platform.
+                ● Design, implemented, monitored and maintained Java micro-services ensuring low-latency.
+                </p>
+                </>
+            ),
+        },
+        {
+            title: "Embedded Software Engineer intern | Amazon | Sep 2022 - Feb 2023 | Berlin, Germany",
+            content: (
+                <>
+                <p>
+                    ● Designed and implemented CAN64 data streaming for an embedded system in C++.
+                </p>
+                <p>
+                    ● Used AWS Lambda to trigger automated processing of incoming protobuf data, optimising data pipeline efficiency.
+                </p>
+                <p>
+                ● Managed the cloud Kotlin code-base and created components to receive protobuf from the embedded system, storing
+                incoming data in Amazon S3.
+                </p>
+                <p>
+                ● Created traffic sign data handling tests using Python, as well as load testing the embedded system.
+                </p>
+                </>
+            ),
+        },
+        {
+            title: "2022 software testing intern @ Nokia",
+            content: (
+                <>
+                <p>
+                    I am so sick and tired of looking for a job im so good please
+                </p>
+                </>
+            ),
+        },
+    ],
+
+};
+
+
+export default SubheadingData;
