b3e38477a2690689b47d5de8e48c043975df8cef
doe1pq <tariqali30021@gmail.com>
2024-10-28 21:45:15 +0100

    Updated a bunch

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import ProjectMenu from "./projectMenu";
 
 export default class Projects extends Component {
   render() {
     return (
-      <div>Projects</div>
-    )
+      <>
+        <ProjectMenu />
+      </>
+    );
   }
 }
---
+++ src/skills/skillsData.js
@@ -0,0 +1,65 @@
+const skills = {
+
+    1: [
+        {
+            title: "HTML",
+            level: 6
+        },
+        {
+            title: "css",
+            level: 5
+        },
+        {
+            title: "SASS",
+            level: 3
+        },
+        {
+            title: "Javascript",
+            level: 5
+        },
+        {
+            title: "Typescript",
+            level: 5
+        },
+        {
+            title: "Tailwind CSS",
+            level: 2
+        },
+        {
+            title: "React JS",
+            level: 5
+        },
+        {
+            title: "Figma",
+            level: 4
+        },
+    ],
+    2: [
+        {
+            title: "SQL",
+            level: 5
+        },
+        {
+            title: "Node.js",
+            level: 4
+        },
+        {
+            title: "MongoDB",
+            level: 4
+        },
+        {
+            title: "Git",
+            level: 6
+        },
+        {
+            title: "PHP",
+            level: 2
+        },
+        {
+            title: "Express.js",
+            level: 4
+        },
+    ]
+}
+
+export default skills;
